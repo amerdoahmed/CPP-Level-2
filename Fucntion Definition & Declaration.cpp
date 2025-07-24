4096a57cb91f12cdc1c596144ce38d82930542a4
Amerdo Ahmed <98106747+amerdoahmed@users.noreply.github.com>
2025-07-24 14:41:57 +0100

    Add files via upload

---
+++ Fucntion Definition & Declaration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// Function Declaration :
+void  add(int, int);
+
+
+
+int main()
+{
+    cout << "C++ Level 2 \n\n";
+
+    add(10, 10);
+}
+
+// Function Definition :
+void add(int a, int b)
+{
+    cout << a + b;
+}
